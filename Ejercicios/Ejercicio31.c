1196f6d1c56936bc3cb1f24d350f6402ea8a885a
german <oligah06@gmail.com>
2025-08-10 22:39:12 +0000

    ejercicio 31 resuelto

---
+++ Ejercicios/Ejercicio31.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main( ) {
+    int a;
+    scanf("%d", &a);
+    if (a>=0) {
+        printf("El numero es positivo\n");
+    } else {
+        printf("El numero es negativo\n");
+    };
+    return 0;
+}
