0647b5a004cdd9530aec8768b25640d0deae58d8
german <oligah06@gmail.com>
2025-08-14 18:58:44 +0000

    correcion

--- Ejercicios/Ejercicio50.c
+++ Ejercicios/Ejercicio50.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int det_num_primo(int num){
-    for(int j=2;j<=num;j++){
-        if(num%j==0 && j!=num){
-            return 0;
-        };
-    };
-    return 1;
-}
+int det_num_primo(int pprim);
 
 int main(){
-    printf("numeros primos entre 1 y 100: ");
+    int num;
+    printf("numeros primos entre 1 y el numero que ingreseses: ");
+    scanf("%i",&num);
+    printf("\n");
 
-    for(int i=0;i<100;++i){
+    for(int i=0;i<num;++i){
         if (i>=2 && det_num_primo(i)==1){
             printf("%i ",i);
         };
     };
+
+    printf("\n");
+}
+
+int det_num_primo(int pprim){
+    for(int j=2;j<=pprim;j++){
+        if(pprim%j==0 && j!=pprim){
+            return 0;
+        };
+    };
+    return 1;
 }
